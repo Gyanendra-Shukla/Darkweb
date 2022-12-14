530d15ad991c9791814dfda6cf074958252197f1
Gyanendra-Shukla <120030497+Gyanendra-Shukla@users.noreply.github.com>
2022-12-14 17:57:17 +0530

    Update sele.py

--- sele.py
+++ sele.py
@@ -8,31 +8,31 @@ driver.get("https://medium.com/tag/technology")
 data = driver.page_source
 alldata = ''.join(data)
 soup = BeautifulSoup(alldata, 'html.parser')
-Head = []
-Link = []
-Content = []
+head = []
+link = []
+content = []
 
-A = soup.find_all('h2', class_='be ek kk kl km kn eo ko kp kq kr es ks kt ku kv ew kw kx ky kz fa la lb lc ld fe ff fg fh fj fk bj')
+heading = soup.find_all('h2', class_='be ek kk kl km kn eo ko kp kq kr es ks kt ku kv ew kw kx ky kz fa la lb lc ld fe ff fg fh fj fk bj')
 
-C = driver.find_elements(By.XPATH, '/html/body/div/div/div/div/div/main/div/div/article/div/div/div/div/div/div/div/div/div/div/div/div/div/div/a')
+hr = driver.find_elements(By.XPATH, '/html/body/div/div/div/div/div/main/div/div/article/div/div/div/div/div/div/div/div/div/div/div/div/div/div/a')
 
-for a in range(len(A)):
-    Head.append(A[a].string)
-for l in range(len(C)):
-    Link.append(C[l].get_attribute('href'))
-for o in Link:
-    driver.get(o)
+for a in range(len(heading)):
+    head.append(heading[a].string)
+for l in range(len(hr)):
+    link.append(hr[l].get_attribute('href'))
+for p in Link:
+    driver.get(p)
     ta = driver.page_source
     aldata = ''.join(ta)
     soup = BeautifulSoup(aldata, 'html.parser')
     D = soup.find_all('div', id="root")
-    tem = ""
+    temp = ""
     for m in range(len(D)):
-        tem += D[m].get_text()
-    Content.append(tem)
-print(len(Content))
-print(len(Link))
-print(len(Head))
+        temp += D[m].get_text()
+    content.append(temp)
+# print(len(content))
+# print(len(link))
+# print(len(head))
 
 w = pd.DataFrame({'Heading': Head, 'Link': Link, 'Content': Content})
 w.to_csv('mediumtech.csv', index=False)
